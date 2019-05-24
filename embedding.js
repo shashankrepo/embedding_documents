ae62ffff944ddf1fb0d1d162581c04148cfdeb40
Shashank Kumar <shsharma93@gmail.com>
2019-05-24 21:06:19 +1000

    first commit

---
+++ embedding.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+//'mongodb://localhost/playground'
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model(
+  'Course',
+  new mongoose.Schema({
+    name: String,
+    author: {
+      type: authorSchema,
+      required: true
+    }
+  })
+);
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() {
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+async function updateCourses(courseId) {
+  const course = await Course.update(
+    { _id: courseId },
+    {
+      $set: {
+        'author.name': 'John Smith'
+      }
+    }
+  );
+}
+
+//createCourse('Node Course', new Author({ name: 'Shas' }));
+//updateCourses('5ce7cf96a642bd1cbcb98148');
+listCourses();
